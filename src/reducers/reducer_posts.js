3a4eb8d32b446d86b1721a23379b8e3580531700
Stanley Louis <stanlouis@msn.com>
2018-09-17 11:58:16 -0400

    Add PostsReducer

---
+++ src/reducers/reducer_posts.js
@@ -0,0 +1,13 @@
+import _ from 'lodash';
+import { FETCH_POSTS } from '../actions';
+
+export default function(state = {}, action) {
+  switch (action.type) {
+    case FETCH_POSTS:
+      // return posts.reduce((acc, val) => Object.assign(acc, {[val['id']] : val}), {})
+      return _.mapKeys(action.payload.data, 'id');
+      break;
+    default:
+      break;
+  }
+}

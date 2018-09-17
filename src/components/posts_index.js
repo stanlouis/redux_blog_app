793fdeb22dc991295891d429f4a59cbd711ad1c6
Stanley Louis <stanlouis@msn.com>
2018-09-17 13:12:10 -0400

    Render the posts

--- src/components/posts_index.js
+++ src/components/posts_index.js
@@ -5,14 +5,27 @@ import { fetchPosts } from '../actions';
 class PostsIndex extends Component {
   componentDidMount = () => {
     this.props.fetchPosts();
-  }
-  
+  };
+
+  renderPosts = () => {
+    return Object.values(this.props.posts).map(post => (
+      <li className="list-group-item" key={post.id}>
+        {post.title}
+      </li>
+    ));
+  };
   render() {
-    return <div>Posts Index</div>;
+    return (
+      <div>
+        <h3>Posts</h3>
+        <ul className="list-group">{this.renderPosts()}</ul>
+      </div>
+    );
   }
 }
 
+const mapStateToProps = state => ({ posts: state.posts });
 export default connect(
-  null,
+  mapStateToProps,
   { fetchPosts }
 )(PostsIndex);
--- src/reducers/reducer_posts.js
+++ src/reducers/reducer_posts.js
@@ -1,14 +1,15 @@
-import _ from 'lodash';
+// import _ from 'lodash';
+// with lodash: return _.mapKeys(action.payload.data, 'id');
 import { FETCH_POSTS } from '../actions';
 
 export default function(state = {}, action) {
   switch (action.type) {
     case FETCH_POSTS:
-      // return posts.reduce((acc, val) => Object.assign(acc, {[val['id']] : val}), {})
-      return _.mapKeys(action.payload.data, 'id');
-      break;
+      return action.payload.data.reduce(
+        (acc, val) => Object.assign(acc, { [val['id']]: val }),
+        {}
+      );
     default:
       return state;
-      break;
   }
 }
